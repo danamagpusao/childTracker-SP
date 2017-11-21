e30c3c37f05ffc003f19e36e4a3c83095de04638
dana <damagpusao@up.edu.ph>
2017-11-21 15:48:47 +0800

    ok

---
+++ app/src/main/java/com/example/ibdnmgps/childtracker2/Utils.java
@@ -0,0 +1,18 @@
+package com.example.ibdnmgps.childtracker2;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Utils{
+
+    private static FirebaseDatabase mDatabase;
+
+    public static FirebaseDatabase getDatabase() {
+        if (mDatabase == null) {
+            mDatabase = FirebaseDatabase.getInstance();
+            mDatabase.setPersistenceEnabled(true);
+        }
+        return mDatabase;
+
+    }
+
+}
